e0d0fdedb865e5308f5fc718f57d757966eb5f9a
10565552046 <1056552046@qq.com>
2025-03-04 16:11:37 +0800

    Compatible with Mek-1.20.1-10.4.15.75.

--- src/main/java/com/jerry/mekanism_extras/common/content/matrix/ExtraMatrixValidator.java
+++ src/main/java/com/jerry/mekanism_extras/common/content/matrix/ExtraMatrixValidator.java
@@ -7,6 +7,7 @@ import it.unimi.dsi.fastutil.longs.Long2ObjectMap;
 import mekanism.common.content.blocktype.BlockType;
 import mekanism.common.lib.multiblock.CuboidStructureValidator;
 import mekanism.common.lib.multiblock.FormationProtocol;
+import mekanism.common.registries.MekanismBlockTypes;
 import mekanism.common.util.WorldUtils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.Block;
@@ -36,9 +37,15 @@ public class ExtraMatrixValidator extends CuboidStructureValidator<ExtraMatrixMu
         if (super.validateInner(state, chunkMap, pos)) {
             return true;
         }
-        if (BlockType.is(state.getBlock(), ExtraBlockType.ABSOLUTE_INDUCTION_CELL, ExtraBlockType.SUPREME_INDUCTION_CELL,
-                ExtraBlockType.COSMIC_INDUCTION_CELL, ExtraBlockType.INFINITE_INDUCTION_CELL, ExtraBlockType.ABSOLUTE_INDUCTION_PROVIDER,
-                ExtraBlockType.SUPREME_INDUCTION_PROVIDER, ExtraBlockType.COSMIC_INDUCTION_PROVIDER, ExtraBlockType.INFINITE_INDUCTION_PROVIDER)) {
+        if (BlockType.is(state.getBlock(),
+                ExtraBlockType.ABSOLUTE_INDUCTION_CELL,
+                ExtraBlockType.SUPREME_INDUCTION_CELL,
+                ExtraBlockType.COSMIC_INDUCTION_CELL,
+                ExtraBlockType.INFINITE_INDUCTION_CELL,
+                ExtraBlockType.ABSOLUTE_INDUCTION_PROVIDER,
+                ExtraBlockType.SUPREME_INDUCTION_PROVIDER,
+                ExtraBlockType.COSMIC_INDUCTION_PROVIDER,
+                ExtraBlockType.INFINITE_INDUCTION_PROVIDER)) {
             //Compare blocks against the type before bothering to look up the tile
             BlockEntity tile = WorldUtils.getTileEntity(world, chunkMap, pos);
             if (tile instanceof ExtraTileEntityInductionCell cell) {
