bf81cad22750f9626d2449026123183cb681fb1a
Lapis <45414333+Lapis256@users.noreply.github.com>
2025-04-07 18:06:52 +0900

    Fix crash at runtime caused by MixinBlockEnergyCube

--- src/main/java/com/jerry/mekanism_extras/mixin/MixinBlockEnergyCube.java
+++ src/main/java/com/jerry/mekanism_extras/mixin/MixinBlockEnergyCube.java
@@ -5,7 +5,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-@Mixin(value = BlockEnergyCube.class, remap = false)
+@Mixin(value = BlockEnergyCube.class)
 public class MixinBlockEnergyCube {
     @ModifyArg(method = "getShape", at = @At(value = "INVOKE", target = "Lmekanism/common/util/WorldUtils;getTileEntity(Ljava/lang/Class;Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;Z)Lnet/minecraft/world/level/block/entity/BlockEntity;"), index = 3)
     private boolean preventWarnLog(boolean original) {
