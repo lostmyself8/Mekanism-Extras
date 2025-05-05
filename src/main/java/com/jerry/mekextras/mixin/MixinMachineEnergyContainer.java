878708ae835726e994515704609e2bbfe9a0b908
10565552046 <1056552046@qq.com>
2025-05-05 17:44:45 +0800

    Correcting the impact of creative upgrade on energy upgrade

--- src/main/java/com/jerry/mekextras/mixin/MixinMachineEnergyContainer.java
+++ src/main/java/com/jerry/mekextras/mixin/MixinMachineEnergyContainer.java
@@ -3,17 +3,19 @@ package com.jerry.mekextras.mixin;
 import com.jerry.mekextras.api.ExtraUpgrade;
 import mekanism.api.AutomationType;
 import mekanism.api.IContentsListener;
+import mekanism.api.Upgrade;
 import mekanism.common.capabilities.energy.BasicEnergyContainer;
 import mekanism.common.capabilities.energy.MachineEnergyContainer;
 import mekanism.common.tile.base.TileEntityMekanism;
+import mekanism.common.util.MekanismUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.function.Predicate;
@@ -31,7 +33,8 @@ public abstract class MixinMachineEnergyContainer<TILE extends TileEntityMekanis
     @Shadow
     public abstract void setMaxEnergy(long maxEnergy);
 
-    @Shadow public abstract long getBaseMaxEnergy();
+    @Shadow
+    public abstract long getBaseMaxEnergy();
 
     protected MixinMachineEnergyContainer(long maxEnergy, Predicate<@NotNull AutomationType> canExtract, Predicate<@NotNull AutomationType> canInsert, @Nullable IContentsListener listener) {
         super(maxEnergy, canExtract, canInsert, listener);
@@ -45,15 +48,25 @@ public abstract class MixinMachineEnergyContainer<TILE extends TileEntityMekanis
         }
     }
 
-    @Inject(method = "updateMaxEnergy", at = @At("RETURN"))
-    public void mixinUpdateMaxEnergy(CallbackInfo ci) {
-        if (tile.supportsUpgrade(ExtraUpgrade.CREATIVE)) {
-           if (tile.getComponent().getUpgrades(ExtraUpgrade.CREATIVE) != 0){
-               setMaxEnergy(Long.MAX_VALUE);
-           }else {
-               setMaxEnergy(getBaseMaxEnergy());
-           }
+//    @Inject(method = "updateMaxEnergy", at = @At("HEAD"))
+//    public void mixinUpdateMaxEnergy(CallbackInfo ci) {
+//        if (tile.getComponent().isUpgradeInstalled(ExtraUpgrade.CREATIVE)) {
+//            setMaxEnergy(Long.MAX_VALUE);
+//        }
+//    }
+
+    /**
+     * @author LostMyself
+     * @reason 兼容创造升级带来的能量变化，不过这会使得兼容性不太高。
+     */
+    @Overwrite
+    public void updateMaxEnergy() {
+        if (tile.supportsUpgrade(Upgrade.ENERGY) || tile.supportsUpgrade(ExtraUpgrade.CREATIVE)) {
+            if (tile.getComponent().isUpgradeInstalled(ExtraUpgrade.CREATIVE)) {
+                setMaxEnergy(Long.MAX_VALUE);
+            } else {
+                setMaxEnergy(MekanismUtils.getMaxEnergy(tile, getBaseMaxEnergy()));
+            }
         }
     }
-
 }
