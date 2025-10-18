6ab7479517772adf0616d93cfee7eb7cf3116a9d
10565552046 <1056552046@qq.com>
2025-10-18 22:33:10 +0800

    Rename

--- src/main/java/com/jerry/mekextras/common/registries/ExtraChemicals.java
+++ src/main/java/com/jerry/mekextras/common/registries/ExtraChemicals.java
@@ -18,7 +18,7 @@ public class ExtraChemicals {
 
     // Gas
     public static final DeferredChemical<Chemical> MOLTEN_THERMONUCLEAR = EXTRA_CHEMICALS.register(ExtraChemicalConstants.MOLTEN_THERMONUCLEAR);
-    public static final DeferredChemical<Chemical> NAQUADAH_TETRAFLUORIDE = EXTRA_CHEMICALS.register(ExtraChemicalConstants.NAQUADAH_HEXAFLUORIDE);
+    public static final DeferredChemical<Chemical> NAQUADAH_HEXAFLUORIDE = EXTRA_CHEMICALS.register(ExtraChemicalConstants.NAQUADAH_HEXAFLUORIDE);
     public static final DeferredChemical<Chemical> FLUORINATED_NAQUADAH_URANIUM_FUEL = EXTRA_CHEMICALS.register(ExtraChemicalConstants.FLUORINATED_NAQUADAH_URANIUM_FUEL);
     public static final DeferredChemical<Chemical> NAQUADAH_URANIUM_FUEL = EXTRA_CHEMICALS.register(ExtraChemicalConstants.NAQUADAH_URANIUM_FUEL);
     public static final DeferredChemical<Chemical> RICH_NAQUADAH_FUEL = EXTRA_CHEMICALS.register(ExtraChemicalConstants.RICH_NAQUADAH_FUEL);
