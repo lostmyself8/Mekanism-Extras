c986e7bf794591cad1c587bb880df70ffe77400b
Jerry <1056552046@qq.com>
2026-05-31 11:39:21 +0800

    删除冗余代码

--- src/main/java/com/jerry/mekanism_extras/client/render/item/block/ExtraRenderEnergyCubeItem.java
+++ src/main/java/com/jerry/mekanism_extras/client/render/item/block/ExtraRenderEnergyCubeItem.java
@@ -5,13 +5,13 @@ import com.jerry.mekanism_extras.client.render.tileentity.ExtraRenderEnergyCube;
 import com.jerry.mekanism_extras.common.item.block.ExtraItemBlockEnergyCube;
 import com.jerry.mekanism_extras.common.tier.ECTier;
 import com.jerry.mekanism_extras.common.tier.TierColor;
-import com.jerry.mekanism_extras.common.tile.ExtraTileEntityEnergyCube;
 
 import mekanism.api.NBTConstants;
 import mekanism.api.RelativeSide;
 import mekanism.client.render.MekanismRenderer;
 import mekanism.client.render.item.MekanismISTER;
 import mekanism.common.lib.transmitter.TransmissionType;
+import mekanism.common.tile.TileEntityEnergyCube.CubeSideState;
 import mekanism.common.tile.component.config.DataType;
 import mekanism.common.util.EnumUtils;
 import mekanism.common.util.ItemDataUtils;
@@ -31,6 +31,8 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
 import org.jetbrains.annotations.NotNull;
 
+import static com.jerry.mekanism_extras.common.tile.ExtraTileEntityEnergyCube.SIDE_STATE_PROPERTY;
+
 public class ExtraRenderEnergyCubeItem extends MekanismISTER {
 
     public static final ExtraRenderEnergyCubeItem EXTRA_RENDERER = new ExtraRenderEnergyCubeItem();
@@ -46,25 +48,25 @@ public class ExtraRenderEnergyCubeItem extends MekanismISTER {
                              int light, int overlayLight) {
         ECTier tier = ((ExtraItemBlockEnergyCube) stack.getItem()).getAdvanceTier();
 
-        ExtraTileEntityEnergyCube.CubeSideState[] sideStates = new ExtraTileEntityEnergyCube.CubeSideState[EnumUtils.SIDES.length];
+        CubeSideState[] sideStates = new CubeSideState[EnumUtils.SIDES.length];
         CompoundTag configData = ItemDataUtils.getDataMapIfPresent(stack);
         if (configData != null && configData.contains(NBTConstants.COMPONENT_CONFIG, Tag.TAG_COMPOUND)) {
             CompoundTag sideConfig = configData.getCompound(NBTConstants.COMPONENT_CONFIG).getCompound(NBTConstants.CONFIG + TransmissionType.ENERGY.ordinal());
             // TODO: Maybe improve on this, but for now this is a decent way of making it not have disabled sides show
             for (RelativeSide side : EnumUtils.SIDES) {
                 DataType dataType = DataType.byIndexStatic(sideConfig.getInt(NBTConstants.SIDE + side.ordinal()));
-                ExtraTileEntityEnergyCube.CubeSideState state = ExtraTileEntityEnergyCube.CubeSideState.INACTIVE;
+                CubeSideState state = CubeSideState.INACTIVE;
                 if (dataType != DataType.NONE) {
-                    state = dataType.canOutput() ? ExtraTileEntityEnergyCube.CubeSideState.ACTIVE_LIT : ExtraTileEntityEnergyCube.CubeSideState.ACTIVE_UNLIT;
+                    state = dataType.canOutput() ? CubeSideState.ACTIVE_LIT : CubeSideState.ACTIVE_UNLIT;
                 }
                 sideStates[side.ordinal()] = state;
             }
         } else {
             for (RelativeSide side : EnumUtils.SIDES) {
-                sideStates[side.ordinal()] = side == RelativeSide.FRONT ? ExtraTileEntityEnergyCube.CubeSideState.ACTIVE_LIT : ExtraTileEntityEnergyCube.CubeSideState.ACTIVE_UNLIT;
+                sideStates[side.ordinal()] = side == RelativeSide.FRONT ? CubeSideState.ACTIVE_LIT : CubeSideState.ACTIVE_UNLIT;
             }
         }
-        ModelData modelData = ModelData.builder().with(ExtraTileEntityEnergyCube.SIDE_STATE_PROPERTY, sideStates).build();
+        ModelData modelData = ModelData.builder().with(SIDE_STATE_PROPERTY, sideStates).build();
         renderBlockItem(stack, displayContext, matrix, renderer, light, overlayLight, modelData);
         double energyPercentage = StorageUtils.getStoredEnergyFromNBT(stack).divideToLevel(tier.getMaxEnergy());
         if (energyPercentage > 0) {
