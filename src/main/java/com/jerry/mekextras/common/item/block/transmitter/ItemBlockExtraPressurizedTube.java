e9fabe5c3bec1afe6aebb2679c95e78d82e17172
Jerry <1056552046@qq.com>
2026-06-26 23:19:50 +0800

    为寰宇支配和悖论无限物品添加变色文字效果

--- src/main/java/com/jerry/mekextras/common/item/block/transmitter/ItemBlockExtraPressurizedTube.java
+++ src/main/java/com/jerry/mekextras/common/item/block/transmitter/ItemBlockExtraPressurizedTube.java
@@ -1,9 +1,11 @@
 package com.jerry.mekextras.common.item.block.transmitter;
 
+import com.jerry.mekextras.common.tier.transmitter.ExtraTransmitterTier;
 import com.jerry.mekextras.common.tier.transmitter.TTier;
 import com.jerry.mekextras.common.tile.transmitter.TileEntityExtraPressurizedTube;
 
 import mekanism.api.text.EnumColor;
+import mekanism.api.text.TextComponentUtil;
 import mekanism.common.MekanismLang;
 import mekanism.common.block.attribute.Attribute;
 import mekanism.common.block.transmitter.BlockSmallTransmitter;
@@ -30,6 +32,10 @@ public class ItemBlockExtraPressurizedTube extends ItemBlockTooltip<BlockSmallTr
     @Override
     public TubeTier getTier() {
         return Objects.requireNonNull(Attribute.getTier(getBlock(), TubeTier.class));
+    }
+
+    public @NotNull Component getName(@NotNull ItemStack stack) {
+        return TextComponentUtil.build(ExtraTransmitterTier.getAdvancedTier(getTier()).getColor(), super.getName(stack));
     }
 
     @Override
